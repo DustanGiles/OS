a1cfc745093ff98078c7e3efe9946140af901067
Dustan Giles <134488476+DustanGiles@users.noreply.github.com>
2025-05-01 12:48:02 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,38 @@
-function startSnake() {
+function startSnake () {
     console.log("Snake game started")
 }
-function startFlappy() {
-    console.log("Flappy game started")
-}
-function showCalculator() {
+// let calcMenu = new Menu(calcButtons, screen.width / 2, screen.height / 2);
+// currentMenu = calcMenu
+function showCalculator () {
     console.log("Calculator selected")
 }
-function selectOutlineColor(colour: number) {
+function Flappy () {
+    console.log("Flappy game started")
+    currentMenu.destroy();
+bird = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . 5 5 5 5 5 5 5 . . . . . . 
+        . . 5 5 5 5 5 5 5 5 5 . . . . . 
+        . 5 5 5 5 5 5 5 5 5 5 5 . . . . 
+        5 5 5 4 4 5 5 5 5 5 5 5 5 . . . 
+        5 5 4 4 4 4 4 5 5 f 5 5 5 . . . 
+        5 4 4 4 4 4 4 4 5 5 5 5 5 4 . . 
+        5 4 4 4 4 4 4 4 5 5 5 5 5 4 4 . 
+        4 4 4 4 4 4 4 5 5 5 5 5 5 4 4 4 
+        4 4 4 4 4 4 5 5 5 5 5 5 5 . . . 
+        4 4 4 4 4 4 5 f 5 5 5 5 5 . . . 
+        . 4 4 4 4 5 5 f f 5 5 5 . . . . 
+        . 4 4 4 5 5 5 5 f f f . . . . . 
+        . . . 5 5 5 5 5 5 5 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+}
+function selectOutlineColor (colour: number) {
     settings.writeNumber("outlineColour", colour)
 }
+let bird: Sprite = null
+let debounceThing = false
 class Text {
     x: number;
     y: number;
@@ -19,7 +42,7 @@ class Text {
         this.x = x;
         this.y = y;
         this.text = textsprite.create(label);
-        this.text.setMaxFontHeight(9);
+        this.text.setMaxFontHeight(8);
         this.text.setOutline(1, outline);
         this.text.setPosition(x, y);
         this.text.z = 10;
@@ -65,13 +88,13 @@ class Menu {
 
     updateSelection(index: number): void {
         let offsetY2 = this.y + (this.y - this.labels[index].y);
-        if (this.oldHeight != this.labels[index].text.height || this.oldWidth != this.labels[index].text.width) {
-            this.selector.fillRect(0, 0, screen.width, screen.height, 0)
-            this.selector.fillRect(this.labels[index].text.x - this.labels[index].text.width / 2, this.labels[index].text.y - this.labels[index].text.height / 2, this.labels[index].text.width, this.labels[index].text.height, 1)
-            scene.setBackgroundImage(this.selector)
-            // this.oldHeight = this.labels[index].text.height
-            // this.oldWidth = this.labels[index].text.width
-        }
+        // if (this.oldHeight != this.labels[index].text.height || this.oldWidth != this.labels[index].text.width) {
+        //     this.selector.fillRect(0, 0, screen.width, screen.height, 0)
+        //     this.selector.fillRect(this.labels[index].text.x - this.labels[index].text.width / 2, this.labels[index].text.y - this.labels[index].text.height / 2, this.labels[index].text.width, this.labels[index].text.height, 1)
+        //     scene.setBackgroundImage(this.selector)
+        //     // this.oldHeight = this.labels[index].text.height
+        //     // this.oldWidth = this.labels[index].text.width
+        // }
 
 
         for (let label2 of this.labels) {
@@ -113,31 +136,31 @@ class Menu {
     handleInput(): void {
         if (controller.up.isPressed()) {
             this.selectPrev();
-            music.play(music.melodyPlayable(music.zapped), music.PlaybackMode.InBackground)
+            //music.play(music.melodyPlayable(music.zapped), music.PlaybackMode.InBackground)
             pause(80);
         }
 
         if (controller.down.isPressed()) {
             this.selectNext();
-            music.play(music.melodyPlayable(music.zapped), music.PlaybackMode.InBackground)
+            //music.play(music.melodyPlayable(music.zapped), music.PlaybackMode.InBackground)
             pause(80);
         }
 
         if (controller.A.isPressed() && debounceThing == false) {
             debounceThing = true
-            music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
+            //music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
             this.activate();
         }
 
         this.updateSelection(this.selectedIndex);
     }
 }
-if (!settings.exists("outlineColour")) {
+if (!(settings.exists("outlineColour"))) {
     settings.writeNumber("outlineColour", 6)
 }
-
-let debounceThing = false;
-
+if (!(settings.exists("textSize"))) {
+    settings.writeNumber("textSize", 9)
+}
 let mainMenuItems: { [key: string]: () => void } = {
     "Calculator": showCalculator,
     "Games": () => openMenu(new Menu(gamesMenuItems, screen.width / 2, screen.height / 2)),
@@ -145,7 +168,7 @@ let mainMenuItems: { [key: string]: () => void } = {
 };
 let gamesMenuItems: { [key: string]: () => void } = {
     "Snake": startSnake,
-    "Flappy": startFlappy,
+    "Flappy": Flappy,
     "Back": () => openMenu(new Menu(mainMenuItems, screen.width / 2, screen.height / 2)),
 };
 let settingsMenuItems: { [key: string]: () => void } = {
@@ -166,21 +189,24 @@ let textOutlineColorMenuItems: { [key: string]: () => void } = {
     "None": () => selectOutlineColor(15),
     "Back": () => openMenu(new Menu(settingsMenuItems, screen.width / 2, screen.height / 2)),
 };
-
-// let audioVolumeMenuItems: { [key: string]: () => void } = {
-//     "20": () => selectAudioVolume(2),
-//     "18": () => selectAudioVolume(3),
-//     "16": () => selectAudioVolume(4),
-//     "14": () => selectAudioVolume(5),
-//     "12": () => selectAudioVolume(6),
-//     "10": () => selectAudioVolume(7),
-//     "8": () => selectAudioVolume(8),
-//     "6": () => selectAudioVolume(9),
-//     "4": () => selectAudioVolume(10),
-//     "2": () => selectAudioVolume(15),
-//     "0": () => openMenu(new Menu(settingsMenuItems, screen.width / 2, screen.height / 2)),
-//     "Back": () => openMenu(new Menu(settingsMenuItems, screen.width / 2, screen.height / 2)),
-// };
+let calcButtons: { [key: string]: () => void } = {
+    "i": () => Flappy(),
+    // "2": () => selectOutlineColor(3),
+    // "3": () => selectOutlineColor(4),
+    // "4": () => selectOutlineColor(5),
+    // "5": () => selectOutlineColor(6),
+    // "6": () => selectOutlineColor(7),
+    // "7": () => selectOutlineColor(8),
+    // "8": () => selectOutlineColor(9),
+    // "9": () => selectOutlineColor(10),
+    // "0": () => selectOutlineColor(15),
+    // "+": () => openMenu(new Menu(settingsMenuItems, screen.width / 2, screen.height / 2)),
+    // "-": () => openMenu(new Menu(settingsMenuItems, screen.width / 2, screen.height / 2)),
+    // "x": () => openMenu(new Menu(settingsMenuItems, screen.width / 2, screen.height / 2)),
+    // "/": () => openMenu(new Menu(settingsMenuItems, screen.width / 2, screen.height / 2)),
+    // "=": () => openMenu(new Menu(settingsMenuItems, screen.width / 2, screen.height / 2)),
+    // "CLEAR ALL": () => openMenu(new Menu(settingsMenuItems, screen.width / 2, screen.height / 2)),
+};
 let currentMenu: Menu;
 let mainMenu = new Menu(mainMenuItems, screen.width / 2, screen.height / 2);
 let gamesMenu: Menu;
